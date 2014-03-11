7baa40f824d3e74ecc50fc99f7d324df1ed92a03
JUN ZHOU <fatman1986@fatman1986-Precision-WorkStation-T7400.(none)>
2014-03-11 11:58:47 -0400

    baseline

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+App1::Application.config.secret_key_base = '55322dab1b379c2394f2b4fc378e6d05572f04d360a19cf4289172a27e9b352f64f1c48d47b13d486dfb9f587d916412bf34fa6202e130a0518f2b0012188b7f'
